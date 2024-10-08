b76494a02532f8cd126a7f7b518a322bf30b8946
Julio <88301276+withjulio@users.noreply.github.com>
2024-10-08 21:06:50 +0700

    Add files via upload

---
+++ Isometric-Testing/Mage Tower Tileset.tsx
@@ -0,0 +1,127 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Mage Tower Tileset" tilewidth="64" tileheight="64" tilecount="41" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="Single_sprites/region_0.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="Single_sprites/region_1.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="Single_sprites/region_2.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="Single_sprites/region_3.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="Single_sprites/region_4.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="Single_sprites/region_5.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="Single_sprites/region_6.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="Single_sprites/region_7.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="Single_sprites/region_8.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="Single_sprites/region_9.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="Single_sprites/region_10.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="Single_sprites/region_11.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="Single_sprites/region_12.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="Single_sprites/region_13.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="Single_sprites/region_14.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="Single_sprites/region_15.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="Single_sprites/region_16.png"/>
+ </tile>
+ <tile id="17">
+  <image width="32" height="32" source="Single_sprites/region_17.png"/>
+ </tile>
+ <tile id="18">
+  <image width="32" height="32" source="Single_sprites/region_18.png"/>
+ </tile>
+ <tile id="19">
+  <image width="32" height="32" source="Single_sprites/region_19.png"/>
+ </tile>
+ <tile id="20">
+  <image width="32" height="32" source="Single_sprites/region_20.png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="32" source="Single_sprites/region_21.png"/>
+ </tile>
+ <tile id="22">
+  <image width="32" height="32" source="Single_sprites/region_22.png"/>
+ </tile>
+ <tile id="23">
+  <image width="32" height="32" source="Single_sprites/region_23.png"/>
+ </tile>
+ <tile id="24">
+  <image width="32" height="32" source="Single_sprites/region_24.png"/>
+ </tile>
+ <tile id="25">
+  <image width="32" height="16" source="Single_sprites/region_25.png"/>
+ </tile>
+ <tile id="26">
+  <image width="32" height="32" source="Single_sprites/region_26.png"/>
+ </tile>
+ <tile id="27">
+  <image width="48" height="64" source="Single_sprites/region_27.png"/>
+ </tile>
+ <tile id="28">
+  <image width="16" height="32" source="Single_sprites/region_28.png"/>
+ </tile>
+ <tile id="29">
+  <image width="32" height="16" source="Single_sprites/region_29.png"/>
+ </tile>
+ <tile id="30">
+  <image width="32" height="16" source="Single_sprites/region_30.png"/>
+ </tile>
+ <tile id="31">
+  <image width="32" height="32" source="Single_sprites/region_31.png"/>
+ </tile>
+ <tile id="32">
+  <image width="32" height="64" source="Single_sprites/region_32.png"/>
+ </tile>
+ <tile id="33">
+  <image width="32" height="32" source="Single_sprites/region_33.png"/>
+ </tile>
+ <tile id="34">
+  <image width="32" height="32" source="Single_sprites/region_34.png"/>
+ </tile>
+ <tile id="35">
+  <image width="64" height="64" source="Single_sprites/region_35.png"/>
+ </tile>
+ <tile id="36">
+  <image width="64" height="64" source="Single_sprites/region_36.png"/>
+ </tile>
+ <tile id="37">
+  <image width="32" height="64" source="Single_sprites/region_37.png"/>
+ </tile>
+ <tile id="38">
+  <image width="32" height="64" source="Single_sprites/region_38.png"/>
+ </tile>
+ <tile id="39">
+  <image width="32" height="32" source="Single_sprites/region_39.png"/>
+ </tile>
+ <tile id="40">
+  <image width="32" height="32" source="Single_sprites/region_40.png"/>
+ </tile>
+</tileset>
